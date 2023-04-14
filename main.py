6acce6ad59f58c20308bfbc484d1ad742710c963
SaadARazzaq <sabdurrazzaq124@gmail.com>
2023-04-14 07:06:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pyttsx3
+from colorama import *
 
 root = tk.Tk()
 root.resizable(False, False)
@@ -19,12 +20,12 @@ def openConsoleInCentre():
 
     # Calculate the coordinates for the top-left corner of the window
     x = (screen_width / 2) - (600 / 2)  # Reuuplace 400 with the width of your window
-    y = (screen_height / 2) - (300 / 2)  # Replace 300 with the height of your window
+    y = (screen_height / 2) - (400 / 2)  # Replace 400 with the height of your window
 
-    root.geometry("600x300+{}+{}".format(int(x), int(y)))
+    root.geometry("600x400+{}+{}".format(int(x), int(y)))
 
 
-def button_clicked(textbox):
+def button_clicked(textbox, output_box):
     sentence = textbox.get("1.0", tk.END)
     obj = pyttsx3.init()
     if not sentence or sentence.isspace():
@@ -34,6 +35,10 @@ def button_clicked(textbox):
     else:
         voice = obj.say(sentence)
         obj.runAndWait()
+        output_box.config(state=tk.NORMAL)
+        output_box.insert(tk.END, " -> Nebula Said : " + sentence)
+        output_box.config(state=tk.DISABLED)
+    textbox.delete('1.0', tk.END)
     return voice
 
 
@@ -42,14 +47,19 @@ def main():
     top_label = tk.Label(root, text="TEXT TO SPEECH CONVERTER!", bg='yellow')
     label = tk.Label(root, text="Enter Your Sentence: ", bg= 'yellow')
     textbox = tk.Text(root, height=0.5, width=50)
-    button = tk.Button(height=1, width=25, text="CONVERT", command=lambda: button_clicked(textbox))
+    button = tk.Button(height=1, width=25, text="CONVERT", command=lambda: button_clicked(textbox, output_box), bg='yellow')
+    output_box = tk.Text(root, height=6, width=50)
+    scrollbar = tk.Scrollbar(root, command=output_box.yview)
+    output_box.config(state=tk.DISABLED, yscrollcommand=scrollbar.set)
 
     # bind Return key to button click
     textbox.bind("<Return>", lambda event: button.invoke())
     top_label.pack(pady=20)
     label.pack(padx=(0, 10), side=tk.LEFT, anchor=tk.N, pady=10)
     textbox.pack(pady=10)
-    button.pack(padx=(5,90), pady=20)
+    button.pack(padx=(5,90), pady=10)
+    output_box.pack(pady=10)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
     root.mainloop()
 
